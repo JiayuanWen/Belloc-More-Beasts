aac0dc7a892a64e6e7eac5ac017629538bb95260
jms9354 <jms9354@psu.edu>
2024-10-24 16:39:34 -0400

    Initial commit

---
+++ docs/stuff.js
@@ -0,0 +1,7 @@
+// stuff.js
+document.addEventListener('DOMContentLoaded', function() {
+    var titleIndex = document.getElementById('title-index');
+    titleIndex.addEventListener('click', function() {
+        alert('Stuff');
+    });
+});
